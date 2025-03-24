e7667c24b3ed85daf4211d7722be932fd19ee4ef
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-03-24 14:04:57 +0100

    Created an automated version of the experiments for testing time results for multiple itterations and averages

---
+++ CPU_tests/automated_cpu_test.py
@@ -0,0 +1,33 @@
+import time
+
+from simple_calculation import *
+from medium_calculation import *
+from long_calculation import *
+
+def log_results(time_list, experiment_name, file_name):
+    average = sum(time_list) / len(time_list)
+
+    with open(f"log_files/{file_name}.txt",'a') as log_file:
+        log_file.write(f"Experiment name = {experiment_name}")
+        log_file.write(f"Raw data: \n")
+        for i in time_list : log_file.write(f"{i}\n")
+        log_file.write(f"Average tasktime over x{len(time_list)} = {average}")
+
+def run_experiment(func, name, runs=10):
+    runtimes = []
+
+    for i in range(runs):
+        start_time = time.time()
+        func()
+        duration = time.time() - start_time
+        runtimes.append(duration)
+    
+    log_results(runtimes, name, file_name="automated_logs")
+    return runtimes
+        
+
+if __name__=='__main__':
+
+    run_experiment(simple_calc, "Simple CPU")     
+    run_experiment(mid_calc, "Mid CPU")
+    run_experiment(long_calc, "Long CPU")
--- CPU_tests/medium_calculation.py
+++ CPU_tests/medium_calculation.py
@@ -1,11 +1,4 @@
 """Medium intensity calculation for CPU time"""
-import time
-
-def log_result(duration):
-    with open("log_files/mid_calc_log.txt", "a") as log_file:
-        log_file.write(f"Task time = {duration}\n")
-
-    print(f"Task time = {duration}\n")
 
 def fibonacci(n):
     if n <= 1:
@@ -13,17 +6,8 @@ def fibonacci(n):
     
     return fibonacci(n - 1) + fibonacci(n - 2)
 
-def medium_calculation(n):
+def mid_calc(n):
     for i in range(n):
         fibonacci(i)
 
 
-if __name__=='__main__':
-
-    start_time = time.time()
-
-    medium_calculation(30)
-
-    duration = time.time() - start_time
-
-    log_result(duration)
