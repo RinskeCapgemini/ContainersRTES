67fdc41918df7f9912c59467aa406cad27c104e1
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-03 14:19:04 +0200

    Added memory restriction flag

--- CPU_tests/E3/long_container/spinning_x.sh
+++ CPU_tests/E3/long_container/spinning_x.sh
@@ -3,7 +3,7 @@
 
 for i in {0..99}; do
     # Change to container name
-    sudo docker run -d -v /home/pirinske/GitHub/ContainersRTES/CPU_tests/E3/long_container/log_files:/host_log_files e2:long_script
+    sudo docker run -d --memory="100m" -v /home/pirinske/GitHub/ContainersRTES/CPU_tests/E3/long_container/log_files:/host_log_files e2:long_script
 
     echo "Created new container"
 
