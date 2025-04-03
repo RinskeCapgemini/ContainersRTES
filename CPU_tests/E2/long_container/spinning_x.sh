ea4f9faa3a44f5f2c3045a41963ff9b701baab45
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-03 12:48:14 +0200

    Remove --rm flags

--- CPU_tests/E2/long_container/spinning_x.sh
+++ CPU_tests/E2/long_container/spinning_x.sh
@@ -3,7 +3,7 @@
 
 for i in {0..99}; do
     # Change to container name
-    sudo docker run --rm -v /home/pirinske/GitHub/ContainersRTES/CPU_tests/E2/long_container/log_files:/host_log_files e2:long_script
+    sudo docker run -v /home/pirinske/GitHub/ContainersRTES/CPU_tests/E2/long_container/log_files:/host_log_files e2:long_script
 
     echo "Created new container"
 
