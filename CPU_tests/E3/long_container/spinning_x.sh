76d3e214f41a6a82d391a10ba14691df2349ea8e
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-03 15:22:45 +0200

    Removed memory flags

--- CPU_tests/E3/long_container/spinning_x.sh
+++ CPU_tests/E3/long_container/spinning_x.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-for i in {0..99}; do
+for i in {0..10}; do
     # Change to container name
-    sudo docker run -d --memory="100m" -v /home/pirinske/GitHub/ContainersRTES/CPU_tests/E3/long_container/log_files:/host_log_files e2:long_script
+    sudo docker run -d -v /home/pirinske/GitHub/ContainersRTES/CPU_tests/E3/long_container/log_files:/host_log_files e2:long_script
 
     echo "Created new container"
 
--- CPU_tests/E3/short_container/spinning_x.sh
+++ CPU_tests/E3/short_container/spinning_x.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-for i in {0..99}; do
+for i in {0..10}; do
     # Change to container name
-    sudo docker run -v /home/pirinske/GitHub/ContainersRTES/CPU_tests/E2/short_container/log_files:/host_log_files e2:simple_script
+    sudo docker run -v -d /home/pirinske/GitHub/ContainersRTES/CPU_tests/E3/short_container/log_files:/host_log_files e2:simple_script
 
     echo "Created new container"
 
