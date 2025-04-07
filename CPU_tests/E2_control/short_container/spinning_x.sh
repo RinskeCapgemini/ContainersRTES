ea1136757143070fbc67f5a0371690261d21978f
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-07 13:22:15 +0200

    New path changes for RPI5 control experiment

--- CPU_tests/E2_control/short_container/spinning_x.sh
+++ CPU_tests/E2_control/short_container/spinning_x.sh
@@ -3,7 +3,7 @@
 
 for i in {0..99}; do
     # Change to container name
-    sudo docker run -v /home/pirinske/GitHub/ContainersRTES/CPU_tests/E2/short_container/log_files:/host_log_files e2:simple_script
+    sudo docker run -v /home/rinske/Github/ContainersRTES/CPU_tests/E2_control/short_container/log_files:/host_log_files short_script:1.0
 
     echo "Created new container"
 
