720565854798cd43b3321746a749603e1e6881a7
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-07 11:40:40 +0200

    Path error

--- CPU_tests/E2_test/long_container/spinning_x.sh
+++ CPU_tests/E2_test/long_container/spinning_x.sh
@@ -3,7 +3,7 @@
 
 for i in {0..10}; do
     # Change to container name
-    sudo docker run -d -v /home/pirinske/GitHub/ContainersRTES/CPU_tests/E2_test/long_container/log_files:/host_log_files long_script:1.0
+    sudo docker run -d -v /home/rinske/GitHub/ContainersRTES/CPU_tests/E2_test/long_container/log_files:/host_log_files long_script:1.0
 
     echo "Created new container"
 
