5767f2a0961f45e99601bf5799177fd7ed57c3eb
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-15 10:33:49 +0200

    Debugging

--- src/experiments/m1/automated_experiment.sh
+++ src/experiments/m1/automated_experiment.sh
@@ -6,9 +6,9 @@ test_name=$1
 host_log_dir="/home/rinske/Github/ContainerRTES/logs/experiments/m1/experiment"  # Host directory for logs
 container_log_dir="/app/logs"  # Container directory for logs
 
-# Python script directory
-host_test_program_dir="/home/rinske/Github/ContainerRTES/src/memory_calculations"  # Host directory containing Python scripts
-host_memory_test_program="/home/rinske/Github/ContainerRTES/src/experiments/m1/memory_test.py"  # Directory containing memory_test.py
+# Python script directories
+host_test_program_dir="/home/rinske/Github/ContainerRTES/src/memory_calculations"  # Host directory containing other Python scripts
+host_memory_test_dir="/home/rinske/Github/ContainerRTES/src/experiments/m1"  # Directory containing memory_test.py
 
 for i in {0..9}; do
     # Run the Docker container with volume mappings for logs and scripts
