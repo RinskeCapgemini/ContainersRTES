6a610bbfd970dedebca45132d6223925a8532b20
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-15 14:00:33 +0200

    Debugging

--- src/experiments/i1/io_control.sh
+++ src/experiments/i1/io_control.sh
@@ -7,7 +7,7 @@ test_name=$1
 
 for i in {0..9}; do
     # Change to experiment name directory
-    python io_test.py $test_name $i control
+    sudo python io_test.py $test_name $i control
 
     echo "Finishing experiment"
 
