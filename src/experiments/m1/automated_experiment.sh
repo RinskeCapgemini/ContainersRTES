2eb68941c40f38193d414505f162a77dd3c3222b
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-15 10:28:12 +0200

    Bug fix?

--- src/experiments/m1/automated_experiment.sh
+++ src/experiments/m1/automated_experiment.sh
@@ -8,12 +8,14 @@ container_log_dir="/app/logs"  # Container directory for logs
 
 # Python script directory
 host_test_program_dir="/home/rinske/Github/ContainerRTES/src/memory_calculations"  # Host directory containing Python scripts
+host_memory_test_program="/home/rinske/Github/ContainerRTES/src/experiments/m1/memory_test.py"  # Directory containing memory_test.py
 
 for i in {0..9}; do
     # Run the Docker container with volume mappings for logs and scripts
     sudo docker run --rm \
         -v "$host_log_dir:$container_log_dir" \
         -v "$host_test_program_dir:/app" \
+        -v "$host_memory_test_dir:/app" \
         general_container:1.0 memory_test.py $test_name $i experiment
 
     echo "Finishing experiment $i"
