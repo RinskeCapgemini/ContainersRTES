1d716b78b34ab4a110ae51d9aa7cd615612d1eb1
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-17 09:40:58 +0200

    UPdated long io script

--- src/io_calculations/io_long.py
+++ src/io_calculations/io_long.py
@@ -2,10 +2,10 @@ import os
 
 
 def io_long(file_path):
-    block = os.urandom(1 * 1024 * 1024)
+    block = os.urandom(10 * 1024 * 1024)
 
     with open(file_path, 'wb') as f:
-        for i in range(50000):
+        for i in range(400000):
             f.write(block)
             f.flush()
             os.fsync(f.fileno())
