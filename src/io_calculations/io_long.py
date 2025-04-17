17c35e4fc43fdaf727d9e10eaf18ae618aa10b90
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-17 10:39:44 +0200

    Updated size of script

--- src/io_calculations/io_long.py
+++ src/io_calculations/io_long.py
@@ -6,7 +6,7 @@ def io_long(file_path):
     block = os.urandom(1 * 1024 * 1024)
 
     with open(file_path, 'wb') as f:
-        for i in range(4000):
+        for i in range(1000):
             f.write(block)
             f.flush()
             os.fsync(f.fileno())
@@ -15,6 +15,8 @@ def io_long(file_path):
 if __name__ == "__main__":
 
     start_time = time.time()
+
+    print(f"Starting time is {start_time:.2f} seconds")
 
     file_path = "/mnt/usb/test_io.txt"
     io_long(file_path)
