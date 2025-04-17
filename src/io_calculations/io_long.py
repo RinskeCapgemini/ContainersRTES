920fe36082dc81dd4c7be44a68064e731d7fc8f9
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-17 10:15:14 +0200

    Solved problem

--- src/io_calculations/io_long.py
+++ src/io_calculations/io_long.py
@@ -4,11 +4,16 @@ import time
 
 
 def io_heavy_random(file_path):
-    file_size = 10 * 1024 * 1024 * 1024  # 10 GB file
+    # Create a large file to perform random writes
+    with open(file_path, 'wb') as f:
+        f.truncate(10 * 1024 * 1024)
+
+
+    file_size = 10 * 1024 * 1024  # 10 GB file
     block_size = 4 * 1024  # 4 KB block (typical disk block size)
 
     with open(file_path, 'r+b') as f:
-        for _ in range(1000000):  # Perform 1,000,000 random writes
+        for _ in range(10000):  # Perform 10,000 random writes
             offset = random.randint(0, file_size - block_size)
             f.seek(offset)
             f.write(os.urandom(block_size))
@@ -23,10 +28,6 @@ if __name__ == "__main__":
     start_time = time.time()
 
     file_path = "/mnt/usb/test_io_random.txt"
-
-    # Create a large file to perform random writes
-    with open(file_path, 'wb') as f:
-        f.truncate(10 * 1024 * 1024 * 1024)  # Pre-allocate 10 GB file
 
     io_heavy_random(file_path)
 
