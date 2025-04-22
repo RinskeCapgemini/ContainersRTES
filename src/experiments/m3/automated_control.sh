bd6b3ecaa30343a09f046fac5e283aef1c1a67b8
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-22 16:07:55 +0200

    Code for experiment m3

---
+++ src/experiments/m3/automated_control.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Run with sudo 
+
+test_name=$1
+
+
+# Log directories
+host_log_dir="/home/rinske/Github/ContainersRTES/logs/memory_logs/m3/"  # Host directory for logs
+container_log_dir="/app/logs"  # Container directory for logs
+
+# Python script directories
+host_memory_test_dir="/home/rinske/Github/ContainersRTES/src/experiments/m3"  # Directory containing memory_test.py
+host_memory_calculations_dir="/home/rinske/Github/ContainersRTES/src/memory_calculations"  # Directory containing memory calculation scripts
+
+# CSV file to store experiment data
+csv_file="${host_log_dir}control/${test_name}_container_time.csv"
+
+# Add CSV header if the file doesn't exist
+if [ ! -f "$csv_file" ]; then
+    echo "Experiment Name,Run Number,Start Time,Finish Time" > "$csv_file"
+fi
+
+
+for i in {0..9}; do
+
+    echo "starting itteration $i"
+
+    # Log the start time
+    start_time=$(date '+%Y-%m-%d %H:%M:%S.%N')  # Human-readable time with nanoseconds
+    
+    # Run the Docker container with volume mappings for logs and scripts
+    sudo docker run --rm \
+        -v "$host_log_dir:$container_log_dir" \
+        -v "$host_memory_test_dir:/app/experiments/m3" \
+        -v "$host_memory_calculations_dir:/app/memory_calculations" \
+        general_container:1.0 /app/experiments/m3/memory_test.py $test_name $i control
+
+    # Log the finish time
+    finish_time=$(date '+%Y-%m-%d %H:%M:%S.%N')  # Human-readable time with nanoseconds
+
+    # Append the data to the CSV file
+    echo "$test_name,$i,$start_time,$finish_time" >> "$csv_file"
+
+
+done
+
+
+sudo unmount /mnt/usb
