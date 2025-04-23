47ee4e53ed48299edcd61cf248857c0210a3d9d0
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-04-23 13:02:58 +0200

    Setup for i3

---
+++ src/experiments/i3/automated_experiment.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Check if the USB is already mounted
+if ! mountpoint -q /mnt/usb; then
+    sudo mount -o sync /dev/sda1 /mnt/usb
+fi
+
+
+# Log directories
+host_log_dir="/home/rinske/Github/ContainersRTES/logs/io_logs/i3/"  # Host directory for logs
+container_log_dir="/app/logs"  # Container directory for logs
+
+# Directories containing code files
+host_io_test_dir="/home/rinske/Github/ContainersRTES/src/experiments/i3"  # Directory containing io_test.py
+
+# External USB directory 
+host_usb_dir="/mnt/usb"  # Host directory for external USB
+container_usb_dir="/app/usb"  # Container directory for external USB
+
+# CSV file to store experiment data container runtimes
+csv_file="${host_log_dir}experiment/experiment_container_time.csv"
+
+
+# Add CSV header if the file doesn't exist
+if [ ! -f "$csv_file" ]; then
+    echo "Experiment Name,Run Number,Start Time,Finish Time" > "$csv_file"
+
+    chmod 777 "$csv_file"  # Make the CSV file writable
+fi
+
+for i in {0..9}; do
+    # Log the start time
+    start_time=$(date '+%Y-%m-%d %H:%M:%S.%N')  # Human-readable time with nanoseconds
+
+    # Run the Docker container with volume mappings for logs, scripts, and USB
+    sudo docker run --rm \
+        -v "$host_log_dir:$container_log_dir" \
+        -v "$host_io_test_dir:/app/experiments/i3" \
+        -v "$host_usb_dir:$container_usb_dir" \
+        general_container:1.0 /app/experiments/i3/read_io.py $i experiment
+
+    # Log the finish time
+    finish_time=$(date '+%Y-%m-%d %H:%M:%S.%N')  # Human-readable time with nanoseconds
+
+    # Append the data to the CSV file
+    echo "$test_name,$i,$start_time,$finish_time" >> "$csv_file"
+done
+
