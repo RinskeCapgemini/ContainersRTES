bbeb17af5879695b156f245a0ebd7189c7c0b026
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-05-01 10:00:48 +0200

    Outcommented wrong line

--- src/experiments/c1.1/automated_control.sh
+++ src/experiments/c1.1/automated_control.sh
@@ -3,7 +3,7 @@
 test_name=$1
 
 for i in {0..9}; do
-    Change to experiment name directory
+#     Change to experiment name directory
     sudo taskset -c 3 python3 cpu_test.py $test_name $i control
 
     echo "Finishing experiment"
