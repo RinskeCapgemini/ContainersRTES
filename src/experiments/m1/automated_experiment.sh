c6b0f59739a9ed87bc6f1c56e34317210896734c
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-05-01 14:25:55 +0200

    Updates for experiment m1: experiment

--- src/experiments/m1/automated_experiment.sh
+++ src/experiments/m1/automated_experiment.sh
@@ -4,10 +4,6 @@
 
 test_name=$1
 
-# Check if the USB is already mounted
-if ! mountpoint -q /mnt/usb; then
-    sudo mount -o sync /dev/sda1 /mnt/usb
-fi
 
 # Log directories
 host_log_dir="/home/rinske/Github/ContainersRTES/logs/memory_logs/m1/"  # Host directory for logs
@@ -46,6 +42,3 @@ for i in {0..9}; do
 
 done
 
-
-
-sudo unmount /mnt/usb
