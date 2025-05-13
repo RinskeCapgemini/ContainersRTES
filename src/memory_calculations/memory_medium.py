3d04b454aacb70378ef1d17fd1ca368c657f347d
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-05-13 09:01:53 +0200

    Updated matrix size to test strange increase in container runtime

--- src/memory_calculations/memory_medium.py
+++ src/memory_calculations/memory_medium.py
@@ -2,10 +2,10 @@ import numpy as np
 import time
 
 def memory_medium():
-    matrix = np.ones((7000, 3600))
+    matrix = np.ones((6000, 3400))
 
     for _ in range(10):
-        _ = matrix[::50]  # Read more rows to simulate more memory activity
+        _ = matrix[::75]  # Read more rows to simulate more memory activity
         # time.sleep(0.5)
 
 if __name__=='__main__':
