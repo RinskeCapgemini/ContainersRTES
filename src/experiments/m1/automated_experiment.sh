b94361f50534c5412e58c910d8fd984d38551313
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-05-13 09:27:51 +0200

    Updated wrong path -> fixed

--- src/experiments/m1/automated_experiment.sh
+++ src/experiments/m1/automated_experiment.sh
@@ -6,7 +6,7 @@ test_name=$1
 
 
 # Log directories
-host_log_dir="/home/rinske/Github/ContainersRTES/logs/memory_logs/m1/"  # Host directory for logs
+host_log_dir="/home/rinske/Github/ContainersRTES/logs/memory_logs/m1/additional_logs/"  # Host directory for logs
 container_log_dir="/app/logs"  # Container directory for logs
 
 # Python script directories
@@ -29,7 +29,7 @@ for i in {0..9}; do
     # Run the Docker container with volume mappings for logs and scripts
     sudo docker run --rm \
         -v "$host_log_dir:$container_log_dir" \
-        -v "$host_memory_test_dir:/app/experiments/m1/additional_testing" \
+        -v "$host_memory_test_dir:/app/experiments/m1" \
         -v "$host_memory_calculations_dir:/app/memory_calculations" \
         general_container:1.0 /app/experiments/m1/memory_test.py $test_name $i experiment
 
