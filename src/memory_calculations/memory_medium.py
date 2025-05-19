c726b0f7e1e77e1b44d0c1f9f609235f7839b121
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-05-19 10:13:54 +0200

    Garbage collection testing

--- src/memory_calculations/memory_medium.py
+++ src/memory_calculations/memory_medium.py
@@ -1,9 +1,10 @@
 import numpy as np 
 import time
-
-
+import gc
 
 def memory_medium():
+    gc.disable()  # Disable garbage collection to prevent it from interfering with memory usage
+    
     matrix = np.ones((6000, 3400))
 
     for _ in range(10):
