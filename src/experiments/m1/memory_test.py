bc6ae18beb60f480412840825053a00ad0c86586
CapgeminiRinske <rinske.heeren@capgemini.com>
2025-05-28 13:14:59 +0200

    added code quality details

--- src/experiments/m1/memory_test.py
+++ src/experiments/m1/memory_test.py
@@ -102,11 +102,15 @@ if __name__=='__main__':
     run_number = int(sys.argv[2])  # Run number of the experiment
     test_type = sys.argv[3]
 
+    if test_type == "experiment":
+        path = r"c:/app/logs"
+    elif test_type == "control":
+        path = r"../../../logs/memory_logs/m1"
 
 
     # Ensure the logs directory exists
-# Logs will be stored in the "logs/memory_logs" directory relative to the project root
-    LOG_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "/app/logs", test_type))              #"../../../logs/memory_logs/m1", /app/logs, ../../../logs/memory_logs/m1/additional_testing
+    # Logs will be stored in the "logs/memory_logs" directory relative to the project root
+    LOG_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), path, test_type))              #"../../../logs/memory_logs/m1", /app/logs, ../../../logs/memory_logs/m1/additional_testing
     os.makedirs(LOG_DIR, exist_ok=True)
 
     # Map experiment names to their corresponding functions
